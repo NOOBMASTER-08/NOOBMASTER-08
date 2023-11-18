706295963363e603e5fe722ee74d8ba683391684
NOOBMASTER-08 <151359300+NOOBMASTER-08@users.noreply.github.com>
2023-11-18 13:59:34 +0530

    Add files via upload

---
+++ print.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    char st;
+    printf("Hello world");
+    scanf("%c",&st);
+    printf("%c",st);
+
+    return 0;
+}
